ada653ca27f475c338806bfcfbfe361face299c5
Onesmus Wambugu <onesmus1024@gmail.com>
2023-05-23 11:08:56 +0300

    fix:fixed receive_messages to enable it to read all messages

--- SQS/main.py
+++ SQS/main.py
@@ -4,11 +4,10 @@ import boto3
 class SQS:
     def __init__(self):
         self.sqs = boto3.resource('sqs')
-        self.available_queues = self.get_all_queues_names()
+        self.available_queues = self._get_all_queues_names()
 
-    def get_all_queues_names(self):
+    def _get_all_queues_names(self):
         return [queue.url.split('/')[-1] for queue in self.sqs.queues.all()]
-    
 
     def create_queue(self, queue_name):
         try:
@@ -19,7 +18,6 @@ class SQS:
         except Exception as e:
             print("Error creating queue:", str(e))
 
-
     def delete_queue(self, queue_name):
         try:
             if queue_name not in self.available_queues:
@@ -29,22 +27,22 @@ class SQS:
         except Exception as e:
             print("Error deleting queue:", str(e))
 
-
     def list_queues(self):
         try:
-           
+
             for queue in self.sqs.queues.all():
                 print("Queue URL: {0}".format(queue.url))
                 print("Queue Name: {0}".format(queue.url.split('/')[-1]))
-                print("Queue ARN: {0}".format(queue.attributes.get('QueueArn')))
+                print("Queue ARN: {0}".format(
+                    queue.attributes.get('QueueArn')))
                 print("Queue Approximate Number of Messages: {0}".format(
                     queue.attributes.get('ApproximateNumberOfMessages')))
-          
+
                 print("="*50)
         except Exception as e:
             print("Error listing queues:", str(e))
-    
-    def message_formated(self,message,queue_name):
+
+    def message_formated(self, message, queue_name):
         print("Queue Name: {0}".format(queue_name))
         print("Message Body: {0}".format(message.body))
         print("Message ID: {0}".format(message.message_id))
@@ -52,7 +50,6 @@ class SQS:
         message.delete()
 
         print("="*50)
-
 
     def send_message(self, queue_name, message):
         try:
@@ -64,34 +61,38 @@ class SQS:
         except Exception as e:
             print("Error sending message:", str(e))
 
-
-    def receive_messages(self, queue_name = None):
+    def receive_messages(self, queue_name=None):
         # get messages from available queues
         try:
             if queue_name is None:
                 # get messages from available queues
                 for queue_name in self.available_queues:
                     queue = self.sqs.get_queue_by_name(QueueName=queue_name)
-                    messages = queue.receive_messages()
-                    for message in messages:
-                        self.message_formated(message,queue_name)
+                    while True:
+                        try:
+                            message = queue.receive_messages()
+                            massage_id = message[0].message_id
+                            self.message_formated(message[0], queue_name)
+                        except Exception as e:
+                            break
             else:
                 # get messages from a specific queue
                 queue = self.sqs.get_queue_by_name(QueueName=queue_name)
-                messages = queue.receive_messages()
-                for message in messages:
-                    self.message_formated(message,queue_name)
+                while True:
+                    try:
+                        message = queue.receive_messages()
+                        massage_id = message[0].message_id
+                        self.message_formated(message[0], queue_name)
+                    except Exception as e:
+                        break
         except Exception as e:
             print("Error receiving messages:", str(e))
-
-        
-
-
 
 
 if __name__ == '__main__':
     sqs = SQS()
 
-    # get messages from onesmus_program_queue
+    # sqs.send_message('onesmus_program_queue', 'Hello test')
+    # sqs.send_message('onesmus_program_queue', 'Hello World')
+    # # get messages from onesmus_program_queue
     sqs.receive_messages('onesmus_program_queue')
-
